0da9392bc637c4daa813458748541964e66b6b4d
rodionmoiseev <rodion.moiseev@gmail.com>
2012-08-04 10:50:28 +0900

    Removed the obsolete in-memory database implementation

--- web/app/dao/Module.scala
+++ web/app/dao/Module.scala
@@ -23,27 +23,3 @@ class MillisecBasedUIDProvider extends UIDProvider{
   val uid: AtomicLong = new AtomicLong(System.currentTimeMillis())
   def nextUID = uid.getAndIncrement()
 }
-
-class InMemoryDB[A <: Persistent[A]] extends DB[A] {
-  var uniqId: AtomicLong = new AtomicLong
-
-  def nextUniqId: Long = uniqId.incrementAndGet
-
-  val items: mutable.MutableList[A] = new mutable.MutableList[A]
-
-  def all = items.toList
-
-  def save(item: A): A = {
-    val newItem = item.withId(nextUniqId)
-    items += newItem
-    newItem
-  }
-
-  def delete(id: Long): A = {
-    throw new UnsupportedOperationException("delete is not supported")
-  }
-}
-
-class InMemoryAssetTasksDB extends InMemoryDB[AssetTask] with AssetTasksDB
-
-class InMemoryAssetsDB extends InMemoryDB[Asset] with AssetsDB
